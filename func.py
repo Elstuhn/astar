68a4f459449eae0e10b7691fb49d501e2cbba657
Poopie <elstuhnofficial@gmail.com>
2022-03-14 08:57:02 +0800

    Update and rename ver1.py to func.py

--- func.py
+++ func.py
@@ -1,5 +1,3 @@
-### Assuming that each step up/down/left/right is 2 units of distance
-
 import numpy as np
 import typing as t
 
@@ -15,18 +13,26 @@ class Node:
     def __eq__(self, node):
         return self.pos == node.pos
 
-def astar(map_ : list, numMap : list, start : tuple = (0, 0), end : tuple = (-1, -1)):
+def astar(map_ : list, numMap : list, start : tuple = (0, 0), end : tuple = None):
     """
     start and end are tuples of node positions
     returns a 2d array containing the optimal path in sequence
     """
+    if not end:
+        end = (len(map_)-1, len(map_[0])-1)
+    if numMap[end[0]][end[1]]:
+        return
+    path = [[-1 for i in range(np.array(map_).shape[1])] for j in range(np.array(map_).shape[0])]
     explored = [map_[start[0]][start[1]]]
     cur_node = map_[start[0]][start[1]]
     gtotal = 0
     cur_node.g = gtotal
     cur_node.h = (((start[0] - end[0]) ** 2) + ((start[1] - end[1]) ** 2))
+    path[cur_node.pos[0]][cur_node.pos[1]] = 0
+    count = 0
     cur_node.updateF()
     while not cur_node == map_[end[0]][end[1]]:
+        count += 1
         gtotal += 2
         explore = []
         position = cur_node.pos
@@ -44,7 +50,19 @@ def astar(map_ : list, numMap : list, start : tuple = (0, 0), end : tuple = (-1,
             if numMap[node.pos[0]][node.pos[1]]:
                 node.f = float("inf")
                 continue
-            elif eval(position) in explored:
+            elif node in explored:
+                continue
+            elif any([
+                all([
+                    not posChecks.index(position),
+                    posX - 1 < 0
+                ]),
+                
+                all([
+                    posChecks.index(position) == 2,
+                    posY - 1 < 0
+                ])
+            ]):
                 continue
             node.g = gtotal
             node.h = (((node.pos[0] - end[0]) ** 2) + ((node.pos[1] - end[1]) ** 2))
@@ -54,36 +72,10 @@ def astar(map_ : list, numMap : list, start : tuple = (0, 0), end : tuple = (-1,
                 lowest = node.f
                 nextNode = node
         cur_node = nextNode
-
-    return backtrack(cur_node, map_)
-
-
-
-def backtrack(end_node : Node, map_) -> list:
-    mapsize = np.array(map_).shape
-    path = [[-1 for i in range(mapsize[1])] for j in range(mapsize[0])]
-    print(path)
-    cur_node = end_node
-    print(cur_node.pos)
-    path[cur_node.pos[0]][cur_node.pos[1]] = 0
-    count = 1
-    while cur_node.parent:
-        cur_node = cur_node.parent
+        explored.append(cur_node)
         path[cur_node.pos[0]][cur_node.pos[1]] = count
-        count += 1
+        
 
     return path
 
-mapr = 5
-mapc = 7
-map_ = [[Node(position = (j, i)) for i in range(mapc)] for j in range(mapr)]
-vmap = \
-    [
-    [0, 1, 1, 0, 0, 1, 0],
-    [0, 0, 0, 1, 1, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 1, 1, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    ]
 
-print(astar(map_, vmap))
